a51c08714b05dec24f91539500765235b27d4541
mac <mac@MacBook-Pro-de-macbook.local>
2020-05-27 14:33:07 +0000

    update setup file

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
-database_name = "casting"
-export DATABASE_URL="postgres://{}/{}".format('localhost:5432', database_name)
-export AUTH0_DOMAIN="fsnds.auth0.com"
+export DATABASE_URL=postgres://normylabftasnx:577b7fa3c895969af939fb905003b1748414e077ddcc95b1535d124d3bcd2af9@ec2-18-206-84-251.compute-1.amazonaws.com:5432/df2surs59k8hpr
+export AUTH0_DOMAIN=fsnds.auth0.com
 export ALGORITHMS=['RS256']
-export API_AUDIENCE = 'castingAgency'
-export ClIENT_ID="RMC2LXQYy6LbiiBOj6g0oWMa0DLaS9MU"
-export ENV="dev"
+export API_AUDIENCE = castingAgency
+export ClIENT_ID=RMC2LXQYy6LbiiBOj6g0oWMa0DLaS9MU
+export ENV=prod
+export FLASK_APP=app.py
+flask run 
