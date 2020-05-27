bc6cd671f73843b13f995fe7f8df4d889c4f0a33
mac <mac@MacBook-Pro-de-macbook.local>
2020-05-27 01:45:11 +0000

    change database path to env

--- backend/database/models.py
+++ backend/database/models.py
@@ -5,8 +5,7 @@ from flask_migrate import Migrate
 import json
 
 database_name = "casting"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
-
+database_path = os.getenv("DATABASE_URL")
 db = SQLAlchemy()
 
 '''
